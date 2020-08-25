32b6e0deae6a424ad7060234a1790d0aedde7edb
wethera <wethera@outlook.com>
2020-08-25 12:27:57 -0500

    updated repo setup from azurehpc

--- specs/default/cluster-init/files/lfsrepo.sh
+++ specs/default/cluster-init/files/lfsrepo.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 lustre_version=${1-2.10}
 
+if [ "$lustre_version" = "2.10" -o "$lustre_version" = "2.12" ]; then
+    lustre_dir=latest-${lustre_version}-release
+else
+    lustre_dir="lustre-$lustre_version"
+fi
+
 cat << EOF >/etc/yum.repos.d/LustrePack.repo
 [lustreserver]
 name=lustreserver
-baseurl=https://downloads.whamcloud.com/public/lustre/latest-${lustre_version}-release/el7/patchless-ldiskfs-server/
-#baseurl=https://downloads.whamcloud.com/public/lustre/lustre-2.12.4/el7/patchless-ldiskfs-server/
+baseurl=https://downloads.whamcloud.com/public/lustre/${lustre_dir}/el7/patchless-ldiskfs-server/
 enabled=1
 gpgcheck=0
-
 [e2fs]
 name=e2fs
 baseurl=https://downloads.whamcloud.com/public/e2fsprogs/latest/el7/
-#baseurl=https://downloads.whamcloud.com/public/e2fsprogs/1.44.5.wc1/el7/
 enabled=1
 gpgcheck=0
-
 [lustreclient]
 name=lustreclient
-baseurl=https://downloads.whamcloud.com/public/lustre/latest-${lustre_version}-release/el7/client/
-#baseurl=https://downloads.whamcloud.com/public/lustre/lustre-2.12.4/el7/client/
+baseurl=https://downloads.whamcloud.com/public/lustre/${lustre_dir}/el7/client/
 enabled=1
 gpgcheck=0
 EOF
